d5f6494ccd0a17dd022e83569ca222efbb580a4d
hong10 <ithong10@126.com>
2015-12-16 20:59:56 +0800

    complete fake neteasy news client, but it have some bugs.

--- app/src/main/java/com/hong/rise/FakeNetEasyNewsActivity.java
+++ app/src/main/java/com/hong/rise/FakeNetEasyNewsActivity.java
@@ -1,50 +1,173 @@
 package com.hong.rise;
 
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.viewpagerindicator.TabPageIndicator;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FakeNetEasyNewsActivity extends FragmentActivity {
-    private static final String[] CONTENT = new String[] { "Recent", "Artists", "Albums", "Songs", "Playlists", "Genres" };
+public class FakeNetEasyNewsActivity extends Activity {
+
+    private int cursorWidth, screenWidth, currentIndex;
+    private TextView tvRecall, tvCreate, tvOffer;
+    private ImageView ivCursor;
+    private ViewPager pager;
+    private List<View> views;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.simple_tabs);
 
-        FragmentPagerAdapter adapter = new GoogleMusicAdapter(getSupportFragmentManager());
+        setContentView(R.layout.activity_fake_net_easy_news);
 
-        ViewPager pager = (ViewPager)findViewById(R.id.pager);
-        pager.setAdapter(adapter);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        screenWidth = dm.widthPixels;
 
-        TabPageIndicator indicator = (TabPageIndicator)findViewById(R.id.indicator);
-        indicator.setViewPager(pager);
+        cursorWidth = screenWidth / 3;
+
+        intTitle();
+        intViewPager();
+
     }
 
-    class GoogleMusicAdapter extends FragmentPagerAdapter {
-        public GoogleMusicAdapter(FragmentManager fm) {
-            super(fm);
+    private void intViewPager() {
+        pager = (ViewPager) findViewById(R.id.pager);
+        views = new ArrayList<View>();
+
+        LayoutInflater inflater = getLayoutInflater();
+        views.add(inflater.inflate(R.layout.viewpager_page1, null));
+        views.add(inflater.inflate(R.layout.viewpager_page2, null));
+        views.add(inflater.inflate(R.layout.viewpager_page3, null));
+
+        pager.setAdapter(new MyPagerAdapter(views));
+        pager.setCurrentItem(0);
+        pager.setOnPageChangeListener(new MyOnPageChangeListener());
+
+
+    }
+
+    private void intTitle() {
+        //init title
+        tvRecall = (TextView) findViewById(R.id.tv_recall);
+        tvCreate = (TextView) findViewById(R.id.tv_create);
+        tvOffer = (TextView) findViewById(R.id.tv_offer);
+
+        tvRecall.setOnClickListener(new MyOnClickListener(0));
+        tvCreate.setOnClickListener(new MyOnClickListener(1));
+        tvOffer.setOnClickListener(new MyOnClickListener(2));
+
+        ivCursor = (ImageView) findViewById(R.id.iv_cursor);
+        //set cursor width
+        ivCursor.getLayoutParams().width = cursorWidth;
+
+
+    }
+
+    private class MyOnClickListener implements View.OnClickListener {
+        private int index = 0;
+
+        public MyOnClickListener(int index) {
+            this.index = index;
         }
 
         @Override
-        public Fragment getItem(int position) {
-            return TestFragment.newInstance(CONTENT[position % CONTENT.length]);
+        public void onClick(View v) {
+            pager.setCurrentItem(index);
+        }
+    }
+
+    /**
+     * MyPagerAdapter
+     */
+    private class MyPagerAdapter extends PagerAdapter {
+        private List<View> views;
+
+
+        public MyPagerAdapter(List<View> views) {
+            this.views = views;
         }
 
         @Override
-        public CharSequence getPageTitle(int position) {
-            return CONTENT[position % CONTENT.length].toUpperCase();
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(views.get(position), 0);
+            return views.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView(views.get(position));
         }
 
         @Override
         public int getCount() {
-            return CONTENT.length;
+            return views.size();
+        }
+
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
         }
     }
+
+
+    public class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+//            Animation animation = new TranslateAnimation(cursorWidth * currentIndex, currentIndex * position, 0, 0);
+            Animation animation = null;
+
+            //init animation
+            switch (position){
+                case 0:
+                    if(currentIndex == 1) {
+                        animation = new TranslateAnimation(cursorWidth, 0, 0, 0);
+                    }else if(currentIndex == 2) {
+                        animation = new TranslateAnimation(2 * currentIndex, 0, 0, 0);
+                    }
+                case 1:
+                    if(currentIndex == 0) {
+                        animation = new TranslateAnimation(0, cursorWidth, 0, 0);
+                    }else if(currentIndex == 2) {
+                        animation = new TranslateAnimation(2 * cursorWidth, cursorWidth, 0, 0);
+                    }
+                case 2:
+                    if(currentIndex == 0) {
+                        animation = new TranslateAnimation(0, 2*cursorWidth, 0, 0);
+                    }else if(currentIndex == 1) {
+                        animation = new TranslateAnimation(2 * cursorWidth, cursorWidth, 0, 0);
+                    }
+            }
+
+            currentIndex = position;
+            animation.setFillAfter(true);
+            animation.setDuration(300);
+            ivCursor.startAnimation(animation);
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+    }
+
+
 }
