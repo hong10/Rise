febf4eec3cfb685ae9003e0eaef8a1703abaca41
hong10 <ithong10@126.com>
2016-03-24 17:20:40 +0800

    complete net util

---
+++ app/src/main/java/com/hong/rise/lottery/GlobalParams.java
@@ -0,0 +1,7 @@
+package com.hong.rise.lottery;
+
+/**
+ * Created by Administrator on 2016/3/24.
+ */
+public class GlobalParams {
+}
---
+++ app/src/main/java/com/hong/rise/utils/HttpClientUtil.java
@@ -0,0 +1,7 @@
+package com.hong.rise.utils;
+
+/**
+ * Created by Administrator on 2016/3/24.
+ */
+public class HttpClientUtil {
+}
