f75167204fd852fb53e1744dc9ab7aeeb9970e4e
hong10 <ithong10@126.com>
2016-06-23 11:20:50 +0800

    Reduce compilation module

---
+++ app/src/main/java/com/hong/rise/SinWaveDemoActivity.java
@@ -0,0 +1,7 @@
+package com.hong.rise;
+
+/**
+ * Created by Administrator on 2016/6/23.
+ */
+public class SinWaveDemoActivity {
+}
---
+++ app/src/main/java/com/hong/rise/view/SineWave.java
@@ -0,0 +1,10 @@
+package com.hong.rise.view;
+
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/6/23.
+ */
+
+public class SineWave extends View implements Runnable {
+}
